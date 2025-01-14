ef9a6c43f6d183847f614038c1be7c3b18811b07
EvilDeeds <evildeedsim@gmail.com>
2025-01-14 21:30:36 +0200

    code update to pandas with read_html

--- ss/All/get_data.py
+++ ss/All/get_data.py
@@ -1,10 +1,11 @@
-import bs4, requests, datetime, csv
+import bs4, requests, datetime, csv # type: ignore
+import pandas as pd # type: ignore
 
 url = 'https://www.ss.lv/ru/real-estate/flats/riga/all/sell/'
 date_now = f'{datetime.datetime.now().date()}'
 symbol = '€'
 
-fields = ['Date',
+cols = ['Date',
         'District',
         'Street',
         'Rooms',
@@ -13,52 +14,9 @@ fields = ['Date',
         'Serie',
         'Price',
         ]
-apartments = []
 
-# districts = ['centre', 
-#             'agenskalns',
-#             'aplokciems',
-#             'bergi',
-#             'bierini',
-#             'bolderaya',
-#             'breksi',
-#             'vecaki',
-#             'vecdaugava',
-#             'vecmilgravis',
-#             'vecriga',
-#             'voleri',
-#             'grizinkalns',
-#             'darzciems',
-#             'daugavgriva',
-#             'dzeguzhkalns',
-#             'dreilini',
-#             'zakusala',
-#             'ziepniekkalns',
-#             'zolitude',
-#             'ilguciems',
-#             'imanta',
-#             'katlakalns',
-#             'kengarags',
-#             'kipsala',
-#             'kliversala',
-#             'kundzinsala',
-#             'mangali',
-#             'mangalsala',
-#             'mezhapark',
-#             'mezhciems',
-#             'plyavnieki',
-#             'purvciems',
-#             'krasta-st-area',
-#             'maskavas-priekshpilseta',
-#             'sarkandaugava',
-#             'teika',
-#             'tornjakalns',
-#             'chiekurkalns',
-#             'shampeteris-pleskodale',
-#             'shkirotava',
-#             'yugla',
-#             'jaunciems',
-#             ]
+
+apartments = []
 
 page_num = 1
 
@@ -66,8 +24,14 @@ page_num = 1
 def add_to_array(array, data):
     array.append(data)
 
+
+full_df = pd.DataFrame()
+
+
 while True:
-    page = requests.get(url + f'page{page_num}.html')
+    print(page_num, end=' ', flush=True)
+    full_url = url + f'page{page_num}.html'
+    page = requests.get(full_url)
     soup = bs4.BeautifulSoup(page.text, 'lxml')
 
     for b in soup.find_all('b'):
@@ -75,47 +39,32 @@ while True:
 
     btn = soup.find_all('button', {'class': 'navia'})[0]
     btn_text = btn.get_text()
+
     if btn_text == '1' and page_num != 1:
         break
+    
+    df = pd.read_html(full_url, header=0, flavor='bs4', index_col=False)
+    table = df[4]
+    table = table.iloc[:, 3:]
 
-    form = soup.find_all('form', {'id': 'filter_frm'})[0]
-    table = form.find_all('table')[2]
-    rows = table.find_all('tr')[1:-1]
-
-    for row in rows:
-        cols = row.find_all('td')[3:]
-        row_arr = []
-        row_arr.append(date_now)
-
-        for col in cols:
-            col_data_arr = [x for x in col.contents if getattr(x, 'name', None) != 'br']
-            if len(col_data_arr) > 1:
-                for c in col_data_arr:
-                    if symbol in c:
-                        c = c.replace(symbol, '').strip()
-                    if len(c) > 0:
-                        c = c.replace(',', '')
-                        add_to_array(row_arr, c)
-            else:
-                c = col_data_arr[0]
-                if symbol in c:
-                        c = c.replace(symbol, '').strip()
-                        c = c.replace(',', '')
-                add_to_array(row_arr, c)
-
-        apartments.append(row_arr)
-        # print(row_arr)
+    full_df = pd.concat([full_df, table], ignore_index=True)
 
     page_num += 1
-    # break
+    # if page_num == 3:
+    #     break
 
-print(len(apartments))
 
+district_street_cols = full_df.iloc[:, 0].str.split(' ', n=1, expand=True)
+full_df.drop(full_df.columns[0], axis=1, inplace=True)
+full_df.insert(0, 0, district_street_cols[0])
+full_df.insert(1, 1, district_street_cols[1])
+full_df['date'] = date_now
+date_col = full_df.pop('date')
+full_df.insert(0, 'date', date_col)
+full_df.columns = cols
+full_df['Price'] = full_df['Price'].str.split(' ', n=1, expand=True)[0].str.replace(',', '').astype(int)
+print(full_df)
 
 path = f'./csv/{date_now}.csv'
-with open(path, 'w', encoding='utf-8', newline='') as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerow(fields)
-    writer.writerows(apartments)
 
-# comment
+full_df.to_csv(path, index=False)
