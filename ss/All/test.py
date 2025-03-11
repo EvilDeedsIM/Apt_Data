187e239cd4900d6262c4ea2b5e1cdb15c7cb59c1
EvilDeeds <61076801+EvilDeedsIM@users.noreply.github.com>
2025-03-11 16:07:59 +0200

    Update test.py

--- ss/All/test.py
+++ ss/All/test.py
@@ -1,7 +1,11 @@
 import time
 import datetime
+import pandas as pd
 
 date_now = f'{datetime.datetime.now().date()}'
 
+full_df = pd.DataFrame()
+full_df['date'] = date_now
+
 path = f'ss/All/csv/{date_now}.csv'
-date_now.to_csv(path, index=False)
+full_df.to_csv(path, index=False)
